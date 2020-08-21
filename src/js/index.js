8c0b735322dea28b7ca9114e884f697391f4a012
Rémi <remi@signalwow.fr>
2020-08-22 00:02:51 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,35 +4,11 @@ import { config } from './config';
 
 const gameConfig = {
     ...config,
-    scene: [main, levelOne, levelTwo, levelThree],
+    scene: [main],
 };
 
 window.onload = function() {
 	const game = new Phaser.Game(gameConfig);
 	game.scene.start({key:'main'});
 }
-
-const levelOne = new Phaser.Scene('levelOne');
-
-levelOne.init = function (data){console.log('tutu')};
-levelOne.preload = function (){};
-levelOne.create = function (data){};
-levelOne.update = function (time, delta){};
-
-const levelTwo = new Phaser.Scene('levelTwo');
-
-levelTwo.init = function (data){console.log('titi')};
-levelTwo.preload = function (){};
-levelTwo.create = function (data){};
-levelTwo.update = function (time, delta){};
-
-const levelThree = new Phaser.Scene('levelThree');
-
-levelThree.init = function (data){console.log('tata')};
-levelThree.preload = function (){};
-levelThree.create = function (data){};
-levelThree.update = function (time, delta){};
-
-let player;
-let cursors;
 
