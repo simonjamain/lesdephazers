ca77153659b3cdd2ce774d3637a626a3345c4f88
Rémi <remi@signalwow.fr>
2020-08-21 23:59:40 +0200

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,60 @@
+import { AUTO } from 'phaser';
+import main from './scenes/main';
+
+const DEFAULT_HEIGHT = window.innerHeight;
+const DEFAULT_WIDTH = window.innerWidth;
+
+const config = {
+	type: AUTO,
+	width: DEFAULT_WIDTH,
+	height: DEFAULT_HEIGHT,
+    pixelArt: true,
+	transparent: false,
+	antialias: false,
+	pixelArt: true,
+	failIfMajorPerformanceCaveat: true,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+    fps: {
+        target: 25,
+        min: 25,
+        forceSetTimeOut: true
+    },
+	canvas: document.querySelector('canvas')
+};
+
+const gameSettings = {
+    player: {
+        speedX: 100,
+        speedY: 70,
+    },
+    score: {
+        bombValue: {
+            default: 20,
+            red: 30,
+            grey: 20,
+        },
+        board: {
+            height: 50,
+            color: 0x121212,
+        },
+        text: {
+            x: 10,
+            y: 10,
+            fontSize: 40,
+        }
+    }
+};
+
+const getSizes = () => {
+    return {
+        width: document.querySelector('#gameContainer > canvas')?.width,
+        height: document.querySelector('#gameContainer > canvas')?.height,
+    };
+}
+
+export { config, gameSettings, getSizes };
---
+++ src/js/scenes/main.js
@@ -0,0 +1,8 @@
+const main = new Phaser.Scene('main');
+
+main.init = function (data){console.log('saperlipopette')};
+main.preload = function (){};
+main.create = function (data){};
+main.update = function (time, delta){};
+
+export default main;
