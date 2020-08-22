ff3d5ec7145b2fa5fd3ef77d744483396877e81d
Pi-L <47693556+Pi-L@users.noreply.github.com>
2020-08-22 12:37:11 +0200

    re re correct package.json

--- src/js/gameClasses/Cell.js
+++ src/js/gameClasses/Cell.js
@@ -1,5 +1,6 @@
 // import { Scene, Input, GameObjects } from 'phaser';
 import CellSprite from './CellSprite';
+import { gameSettings } from '../config';
 
 export default class Cell {
 	constructor(exNihilo, scene, x, y, action1, action2) {
@@ -7,12 +8,13 @@ export default class Cell {
 		this.exNihilo = exNihilo;
 		this.x = x;
 		this.y = y;
+		this.side = gameSettings.grid.cellSide;
 		this.action1 = action1;
 		this.action2 = action2;
 
 		this.player = null;
 
-		this.cellSprite = new CellSprite(scene, this, x, y, this.player?.color);
+		this.cellSprite = new CellSprite(scene, this, x, y, this.side, this.player?.color);
 	}
 
 	// exNihilo.doAction(action);
--- src/js/gameClasses/CellSprite.js
+++ src/js/gameClasses/CellSprite.js
@@ -3,7 +3,7 @@ import { Scene, Input, GameObjects } from 'phaser';
 class CellSprite extends GameObjects.Sprite {
 	constructor(scene, cell, x, y, color, scale = 1) {
 
-		super(scene, x, y, type);
+		super(scene, x, y);
 
 		this.color = color !== null ? color : '#fff'
 
