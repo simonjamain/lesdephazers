1e205f30801a73fb29aa42b584fa2d924ad91d19
Rémi <remi@signalwow.fr>
2020-08-22 10:38:32 +0200

    exNihilo to ExNihilo

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import { Game } from 'phaser';
 import MainScene from './scenes/MainScene';
 import { config } from './config';
-import { ExNihilo } from './gameClasses/exNihilo';
+import { ExNihilo } from './gameClasses/ExNihilo';
 
 const exNihilo = new ExNihilo();
 exNihilo.init(10, 10);
