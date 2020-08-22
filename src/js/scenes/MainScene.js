ca1a9aed71f57ee4006d36c2597a9ed03c5ef054
Pi-L <47693556+Pi-L@users.noreply.github.com>
2020-08-22 10:13:17 +0200

    refacto main scene & index.js

---
+++ src/js/scenes/MainScene.js
@@ -0,0 +1,26 @@
+import { Scene, Input } from 'phaser';
+
+
+export default class MainScene extends Scene {
+	constructor({ key }) {
+		super({ key: 'mainScene' });
+
+	}
+
+	init(data) {
+		console.log('saperlipopette');
+	}
+
+	preload() {
+
+	}
+
+	create(data) {
+
+	}
+
+	update(time, delta) {
+
+	}
+}
+
--- src/js/scenes/main.js
+++
@@ -1,8 +0,0 @@
-const main = new Phaser.Scene('main');
-
-main.init = function (data){console.log('saperlipopette')};
-main.preload = function (){};
-main.create = function (data){};
-main.update = function (time, delta){};
-
-export default main;
