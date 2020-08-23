82b9ff4bc800f63a2fda10438e3074f07aeeddb2
Pi-L <47693556+Pi-L@users.noreply.github.com>
2020-08-23 18:59:07 +0200

    pointer none

--- src/js/gameClasses/Player.js
+++ src/js/gameClasses/Player.js
@@ -27,55 +27,6 @@ export default class Player {
     }
 
 
-
-    // addBall = () => {
-
-    // }
-
-    // setCursorCircle = () => {
-    //     this.munitionsGraphics = this.exNihilo.scene.add.group();
-    //     this.cursorCircle = new Geom.Circle(0, 0, 50);
-    //     this.graphicsCursorCircle = this.exNihilo.scene.add.graphics();
-    //     this.graphicsCursorCircle.setStrokeStyle(1, this.color);
-    //     this.graphicsCursorCircle.strokeCircleShape(this.cursorCircle);
-    // }
-
-    // updateCursorCircle = (e) => {
-    //     this.cursorCircle.x = e.x;
-    //     this.cursorCircle.y = e.y;
-    // }
-
-    // setCircles = () => {
-    // for (let i = 0; i < this.nbMunitionLeft; i++) {
-    // const graphics = this.exNihilo.scene.add.graphics();
-    // graphics.fillStyle(this.color);
-    // graphics.setStrokeStyle(1, this.color)
-    // const circle = new Phaser.Geom.Circle(0, 0, 15);
-    // graphics.fillCircleShape(circle);
-    // graphics.strokeCircleShape(circle);
-
-    // this.munitionsGraphics.push();
-
-    // }
-    // }
-
-    // createCircle = () => {
-    //     for (let i = 0; i < this.nbMunitionLeft; i++) {
-    //         const graphics = this.exNihilo.scene.add.graphics();
-    //         graphics.fillStyle(this.color);
-    //         const circle = new Phaser.Geom.Circle(500, 500, 20);
-    //         graphics.fillCircleShape(circle);
-
-    //         this.munitionsGraphics.push();
-
-    //     }
-
-    //     // this.exNihilo.scene.add.existing(circle);
-
-
-    // }
-
-
     pointerMove = (pointer) => {
         // console.log('pointer.x', pointer.x)
     }
