51668828ff7e32fe82a8e6373af2ad4aa9a6ee24
Rémi <remi@signalwow.fr>
2020-08-23 16:44:21 +0200

    Add force to bonuxify for multiplayer purpose

--- src/js/gameClasses/CellIterationRule.js
+++ src/js/gameClasses/CellIterationRule.js
@@ -133,15 +133,14 @@ export default class CellIterationRule {
         return {nbNeihbor: nbNeihbor, counter: counter};
     }
 
-    bonuxify = (cell, rand = Math.random()) => {
-        if (Math.random() < 0.00005 + this.exNihilo.players.length / 100000)
+    bonuxify = (cell, force = false, rand = Math.random()) => {
+        if (force || Math.random() < 0.00005 + this.exNihilo.players.length / 100000)
         {
             cell.futurPlayer = new Player(this.exNihilo, 0x010101);
             cell.special = true;
             cell.rand = rand;
             cell.action1 = this.exNihilo.cellActionRule['random'];
             cell.action2 = cell.action1;
-            console.log('bonux');
         }
     }
 
