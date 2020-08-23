3e98e8acbae51cd1154d4d78f948c7a656614172
Simon J <jamain.simon@gmail.com>
2020-08-23 18:59:45 +0200

    added crosshair and it was easy

--- src/js/scenes/MainScene.js
+++ src/js/scenes/MainScene.js
@@ -24,7 +24,9 @@ export default class MainScene extends Scene {
 	}
 
 	create(data) {
-		
+		this.crosshairGraphics = this.add.graphics()
+		document.crosshairGraphics = this.crosshairGraphics
+
 		this.cellConvertionEmitter = this.add.particles('spark').createEmitter({
             speed: { min: -300, max: 300 },
             angle: { min: 0, max: 360 },
@@ -49,6 +51,44 @@ export default class MainScene extends Scene {
 	update(time, delta) {
 		this.exNihilo.elapsedTime = Math.floor(time / 1000);
 		this.exNihilo.infosBoard.updateTime();
+		
+		this.crosshairGraphics.clear()
+		this.drawCrosshair()
+	}
+
+	drawCrosshair(){
+
+		let realPlayer = this.exNihilo.findPlayer(this.exNihilo.player.color)
+		var color = realPlayer.color;
+		var thickness = 3;
+		var alpha = 1;
+		this.crosshairGraphics.lineStyle(thickness, color, alpha);
+		this.crosshairGraphics.fillStyle(color, alpha);
+	
+		var a = new Phaser.Geom.Point(this.game.input.mousePointer.x, this.game.input.mousePointer.y);
+
+		// outer
+		this.crosshairGraphics.strokeCircle(a.x, a.y, 55);
+
+		//inner
+		this.crosshairGraphics.fillCircle(a.x, a.y, 2);
+
+		let startingAngle = 4 * (Math.PI / 6)
+		let angleInterMunition = (Math.PI / 6)
+		let distance = 80
+
+		for (let munitionIndex = 0; munitionIndex < realPlayer.nbMunitionMax; munitionIndex++) {
+
+			let xCoord = a.x + Math.cos(startingAngle + angleInterMunition * munitionIndex) * distance
+			let yCoord = a.y + Math.sin(startingAngle + angleInterMunition * munitionIndex) * distance
+
+			if(munitionIndex < realPlayer.nbMunitionLeft)
+			{
+				this.crosshairGraphics.fillCircle(xCoord,yCoord,15)
+			}else{
+				this.crosshairGraphics.strokeCircle(xCoord,yCoord,15)
+			}
+		}
 	}
 }
 
